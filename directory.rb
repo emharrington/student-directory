332bed70dedb090b3cd69f83825af880831943e3
your name <your email>
2022-09-08 13:12:42 +0100

    refactoring

--- directory.rb
+++ directory.rb
@@ -1,24 +1,66 @@
+@students = [] # empty array accessible by all methods
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
-  # get the fist name
+  # get the fist name:
   name = gets.chomp
   # when name isnt empty, repeat this code:
   while !name.empty? do
-    # add the student hash to array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    # add the student hash to array:
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     # get another name from user:
     name = gets.chomp
   end
-  #return the array of students:
-  students
 end
 
-# then, we call our methods:
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 as we will add more items
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again."
+  end
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_students_list
+  # for every student iteration, we get their name & cohort:
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+menu
