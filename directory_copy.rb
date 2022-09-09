4dcb487bfcdc769a647038e75b26e6ba0fa3f15b
your name <your email>
2022-09-09 22:15:10 +0100

    ex 6

--- directory_copy.rb
+++ directory_copy.rb
@@ -1,14 +1,15 @@
 @students = []
+@width = 50
 
 def new_student
-  puts "Create new student? (y/n)"
+  puts "Create new student? (y/n)".center(@width)
   answer = gets.chomp.downcase
   if answer == "y"
     return true
   elsif answer == "n"
     return false
   else
-    puts "Create new student? (y/n)"
+    puts "Create new student? (y/n)".center(@width)
   end
   new_student
 end
@@ -16,33 +17,33 @@ end
 def input_students
   answer = new_student
   while answer
-    puts "Please enter students name:"
+    puts "Please enter students name:".center(@width)
     name = gets.chomp
-    puts "Please enter students country of birth:"
-    country_of_birth = gets.chomp
-    puts "Please enter students hobbies:"
+    puts "Please enter students country of birth:".center(@width)
+    country = gets.chomp
+    puts "Please enter students hobbies:".center(@width)
     hobbies = gets.chomp
-    @students << { name: name, cohort: :november, country_of_birth: country_of_birth, hobbies: hobbies }
-    puts "We now have #{@students.count} students"
+    @students << {name: name, cohort: :november, country: country, hobbies: hobbies}
+    puts "We now have #{@students.count} students".center(@width)
     answer = new_student
   end
   @students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(@width)
+  puts "-------------".center(@width)
 end
 
 def print_student
-  puts "NAME - COHORT - COUNTRY - HOBBIES"
+  puts "NAME, COHORT, COUNTRY, HOBBIES".center(@width)
   @students.each do |student|
-    puts "#{@students[:name]} - #{@students[:cohort]} - #{@students[:country_of_birth]} - #{@students[:hobbies]}"
+    puts "#{@students[:name]} #{@students[:cohort]} #{@students[:country]} #{@students[:hobbies]}".center(@width)
   end
 end
 
 def print_footer
-  puts "Overall, we have #{@students.count} great students"
+  puts "Overall, we have #{@students.count} great students".center(@width)
 end
 
 @students = input_students
